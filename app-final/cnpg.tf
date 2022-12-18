9701d84220e5c49092b3d683a7f3b79aa7e711d1
Sam Wight <samuelwight@gmail.com>
2022-12-18 14:22:35 -0600

    Fix name

--- app-final/cnpg.tf
+++ app-final/cnpg.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "cnpg_operator" {
   repository = "https://cloudnative-pg.github.io/charts"
-  chart = "cnpg/cloudnative-pg"
+  chart = "cnpg"
   namespace = "cnpg-system"
   create_namespace = true
   name = "cnpg"
